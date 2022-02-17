f458262586f926f2670f95b9c4257565be34652b
SCBalate <67940266+SCBalate@users.noreply.github.com>
2022-02-17 14:34:06 +0530

    Added reactive forms validation

--- Route-authentication-and-protection/src/app/auth/auth.component.ts
+++ Route-authentication-and-protection/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+ 
+ Form!: FormGroup;
+  loginMode:boolean = true;
+  constructor(private fb:FormBuilder) { 
+    this.createForm()
+  }
+  createForm(){
+    this.Form = this.fb.group({
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(8)]]
+    });
   }
 
+  ngOnInit(): void {
+  
+  }
+
+  onSwitchMode(){
+    this.loginMode = !this.loginMode;
+  }
+
+  onSubmit(){
+    if(this.Form.valid){
+      console.log(this.Form.value);
+    }else{  
+    console.log('This form is invalid');
+    
+  }
+}
 }
